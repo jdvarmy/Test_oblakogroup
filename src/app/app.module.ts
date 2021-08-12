2340e897d95c3a2453a0d66ad777517e81ee6be9
sergey.antonov <sergey.antonov@nedra.digital>
2021-08-12 10:39:41 +0300

    feat(noIssue): initial app
    initial app
    
    Closes noIssue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { ButtonComponent } from './ui/button/button.component';
+import { TableComponent } from './ui/table/table.component';
+import { TextComponent } from './ui/text/text.component';
+import { InputComponent } from './ui/input/input.component';
+import { InputSearchComponent } from './ui/input-search/input-search.component';
+import { InputListComponent } from './ui/input-list/input-list.component';
+import { IconComponent } from './ui/icon/icon.component';
+import { RadioGroupComponent } from './ui/radio-group/radio-group.component';
+import { CheckBoxGroupComponent } from './ui/check-box-group/check-box-group.component';
+import { ContextMenuComponent } from './ui/context-menu/context-menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TableComponent, TextComponent, InputComponent, InputSearchComponent, InputListComponent, IconComponent, RadioGroupComponent, CheckBoxGroupComponent, ContextMenuComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
